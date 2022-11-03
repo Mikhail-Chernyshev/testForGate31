5b7426892ac970b9a90cc99b5f94c95fc5525bb0
Mikhail Chernyshev <newmishach@gmail.com>
2022-11-03 13:15:19 +0400

    functional done

--- index.js
+++ index.js
@@ -20,17 +20,15 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-// const emptyParams = new URLSearchParams();
-// const paramsFromUrl = new URLSearchParams(window.location.search);
+const emptyParams = new URLSearchParams();
+const paramsFromUrl = new URLSearchParams(window.location.search);
 console.log(window.location.search);
-// const params = new URLSearchParams({ inputvalue });
 function createSearch(value) {
   const params = new URLSearchParams('a[]');
   params.set('a[]', value);
-
-  //   params.append(value, value);
 }
 function searchApi(url) {
+  window.location = 'pipka';
   arrayTitles.forEach((el) => {
     el.parentNode.style.display = 'none';
   });
@@ -58,7 +56,6 @@ function searchApi(url) {
           append(div, title);
           append(div, text);
           append(div, checkbox);
-          // checkSearch();
         }
       });
       createSearch(input.value);
@@ -67,51 +64,4 @@ function searchApi(url) {
       console.log(error);
     });
 }
-function getItems(url) {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      data.map((element) => {
-        let div = createNode('div');
-        div.classList.add('element');
-        let title = createNode('h3');
-        title.classList.add('element_title');
-        let text = createNode('p');
-        let checkbox = createNode('input');
-        checkbox.type = 'checkbox';
-        checkbox.classList.add('checkbox');
-        checkbox.addEventListener('click', () => {
-          div.classList.toggle('onClick');
-        });
-        arrayTitles.push(title);
-        title.innerHTML = element.title;
-        text.innerHTML = element.body;
-        append(ul, div);
-        append(div, title);
-        append(div, text);
-        append(div, checkbox);
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-// function search(array) {
-//   let input = document.getElementById('input-search');
-//   if (input.value === '') {
-//     array.forEach((element) => {
-//       element.parentNode.style.display = 'block';
-//       element.style.display = 'block';
-//     });
-//   } else {
-//     array.forEach((element) => {
-//       if (element.textContent.includes(input.value)) {
-//         element.style.display = 'block';
-//       } else {
-//         element.parentNode.style.display = 'none';
-//         element.style.display = 'hidden';
-//       }
-//     });
-//   }
-// }
-getItems(url);
+searchApi(url);
